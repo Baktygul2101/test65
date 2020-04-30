edd9bfe1597b141c6570119c0b80798265e276ba
Baktygul <bakulyakozhoshova@gmail.com>
2020-04-30 17:55:55 +0600

    11

--- src/main/resources/db/migration/ V1000__products_and_categories_tables.sql
+++ src/main/resources/db/migration/ V1000__products_and_categories_tables.sql
@@ -16,14 +16,7 @@ create table `categories` (
  PRIMARY KEY (`id`)
 );
 
-create table `users` (
- `id` INT auto_increment NOT NULL,
- `name` varchar(128) NOT NULL,
- `email` varchar(128) NOT NULL,
- `login` varchar(128) NOT NULL,
- `password` varchar(128) NOT NULL,
- PRIMARY KEY (`id`)
-);
+
 
 insert into `categories` (`name`, `icon`) values ('Coffee', 'hot_beverage'), ('Steak', 'steaks'),
                                                  ('Burger', 'burgers'), ('Bakery', 'bakery'),
